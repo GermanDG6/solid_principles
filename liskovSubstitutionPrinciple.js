bff11399de923c5481282d044dd112434dc3de48
GermanDG <gdelgado@civitfun.com>
2022-09-12 18:11:32 +0100

    practising SOLID principles (LSP)

---
+++ liskovSubstitutionPrinciple.js
@@ -0,0 +1,42 @@
+//Ejercicio para practicar el principio de sustitución de Liskov (soLid principles)
+
+//Clase padre
+class Bird {
+  tweet(name) {
+    return `${name} PIO`;
+  }
+  canFly() {
+    return "YES";
+  }
+}
+
+//Subclases
+
+// Cumplen LSP
+class Pigeon extends Bird {
+  tweet() {
+    return `${name} PIO`;
+  }
+}
+
+//No cumplen LSP
+class Chicken extends Bird {
+  canFly() {
+    return "NO";
+  }
+}
+
+class Goat extends Bird {
+  bee(name) {
+    return `${name} beeeeeeee`;
+  }
+}
+
+let pajaro = new Bird();
+let pollo = new Chicken();
+let cabra = new Goat();
+
+console.log(pajaro.tweet("german"));
+console.log(pollo.canFly());
+console.log(cabra.tweet("Silvia"));
+console.log(cabra.bee("Silvia"));
